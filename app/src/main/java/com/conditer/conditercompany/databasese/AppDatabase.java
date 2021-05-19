7fe8433da4ca09e7240df193b52477721f1da505
feepro1 <79868628+feepro1@users.noreply.github.com>
2021-05-19 15:34:02 +0300

    Update AppDatabase.java

--- app/src/main/java/com/conditer/conditercompany/databasese/AppDatabase.java
+++ app/src/main/java/com/conditer/conditercompany/databasese/AppDatabase.java
@@ -2,7 +2,7 @@ package com.conditer.conditercompany.databasese;
 
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
-
+//абстрактный класс объекта бд с объявлением всех интерфейсов для работы с таблицами
 @Database(entities = {userTable.class,priceTable.class,buyTable.class}, version = 8)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract userTableDao userTableDao();
