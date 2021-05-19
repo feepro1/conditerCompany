d252da4cc27fd740ad2c629d11c01de081618da7
feepro1 <79868628+feepro1@users.noreply.github.com>
2021-05-19 15:14:34 +0300

    Update buyActivity.kt

--- app/src/main/java/com/conditer/conditercompany/buyActivity.kt
+++ app/src/main/java/com/conditer/conditercompany/buyActivity.kt
@@ -14,7 +14,7 @@ import com.conditer.conditercompany.databasese.buyTable
 import com.conditer.conditercompany.databasese.userTable
 
 class buyActivity : AppCompatActivity() {
-
+//класс с эраном покупки
         lateinit var db: AppDatabase
         private lateinit var countTW :TextView
         private lateinit var summTW :TextView
@@ -24,7 +24,7 @@ class buyActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_buy)
         price = intent.getLongExtra("pricePrice",-1)
-
+//определяем view для количества и суммарной стоимости покупки
         countTW =  findViewById<TextView>(R.id.countTW)
         summTW =  findViewById<TextView>(R.id.summTW)
 
@@ -38,30 +38,31 @@ class buyActivity : AppCompatActivity() {
             findViewById<ImageView>(R.id.imageView3).setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_candy))
         }
     }
-
+ //убрать один товар из покупки
     fun minusBuyBtnClick(view: View) {
         countTW.text = if (countTW.text.toString().toInt()>0)(countTW.text.toString().toInt()-1).toString() else 0.toString()
         summTW.text = if (countTW.text.toString().toInt()>0)(countTW.text.toString().toInt()*price).toString()else price.toString()
     }
+//добавить один товар в покупку
     fun addBuyBtnClick(view: View) {
         countTW.text = (countTW.text.toString().toInt()+1).toString()
         summTW.text = (summTW.text.toString().toInt()+price).toString()
     }
-
+//совершить покупку
     fun buyBtnClick(view: View) {
         db = Room.databaseBuilder(
                 applicationContext,
                 AppDatabase::class.java, "database"
         ).allowMainThreadQueries().fallbackToDestructiveMigration().build()
         val mySharedPreferences = getSharedPreferences("prefer", Context.MODE_PRIVATE)
-
+//определение интерфейса для работы с бд
         val btd = db.buyTableDao()
         val newBuy =  buyTable()
         newBuy.id_price = intent.getLongExtra("id_price",-1)
         newBuy.rateBuy = findViewById<RatingBar>(R.id.ratingBar2).rating.toLong()
         newBuy.id_user = mySharedPreferences.getLong("user_id_SP",0)
 
-        btd.addNewBuy(newBuy)
-        finish()
+        btd.addNewBuy(newBuy)//добавление записей в таблицу покупок
+        finish()//закрытие активити
     }
 }
