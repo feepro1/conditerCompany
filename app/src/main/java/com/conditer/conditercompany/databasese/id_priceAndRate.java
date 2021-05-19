458fc3dde04b0e94af69fa207a69db4b9ce4f3e7
feepro1 <79868628+feepro1@users.noreply.github.com>
2021-05-19 15:35:11 +0300

    Update id_priceAndRate.java

--- app/src/main/java/com/conditer/conditercompany/databasese/id_priceAndRate.java
+++ app/src/main/java/com/conditer/conditercompany/databasese/id_priceAndRate.java
@@ -1,5 +1,5 @@
 package com.conditer.conditercompany.databasese;
-
+//объект для удобной передачи соответствия id в таблице и его рейтинга
 public class id_priceAndRate {
     public long id_price;
     public long rateBuy;
